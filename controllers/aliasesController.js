35ccbdf43233a7fe65512f920cd7e722671f92f6
seanvsville <seanvanho@gmail.com>
2016-04-08 09:53:41 -0700

    completed post route edge case to add confession to existing alias

--- controllers/aliasesController.js
+++ controllers/aliasesController.js
@@ -14,7 +14,6 @@ function index(req, res) {
 
 // POST /api/aliases
 function create(req, res) {
-  //console.log('what is my input?', req.body);
 
   // set the value of the new Alias
   var newAlias = {
@@ -23,36 +22,38 @@ function create(req, res) {
   };
 
   db.Alias.find(newAlias, function isFound(err, found) {
-    if(err) {return console.log('ERR1: ', err);}
+    if (err) {return console.log('ERR1: ', err);}
     // if found is empty, this is new alias
     if(found.length===0) {
       console.log('This is a new alias, creating a new db entry...');
       db.Alias.create(newAlias, function newAliasCreated(err, createdAlias) {
-        if(err) {return console.log('ERR2: ', err);}
+        if (err) {return console.log('ERR2: ', err);}
         console.log('Created new Alias');
         createdAlias.confessions.push(new db.Confession({submission: req.body.confession }));
         console.log('New Confessions: ', createdAlias.confessions);
         createdAlias.save(function sendJSON(err, finalAlias) {
-          if(err) {return console.log('ERR3: ', err);}
+          if (err) {return console.log('ERR3: ', err);}
           console.log('Successfully added confession and sending whole new alias');
           res.json(finalAlias);
         });
       });
     }
-    // if alias already exists, attach confession to existing alias
+    // if found (alias) already exists, attach confession to existing alias
     else if (found[0]._id) {
+      var aliasId = found[0]._id;
+      console.log('req.params.id: ', aliasId);
       var newConfession = req.body.confession;
       console.log('req.body.confession: ', newConfession);
-      db.Alias.create(newConfession, function newConfessionCreated(err, addedConfession) {
-        if(err) {return console.log("ERR2: ", err);}
-        console.log("Created new Alias: ", addedConfession);
-/*        addedConfession.confessions.push(new db.Confession({submission: req.body.confession }));
-        console.log('Added Confession: ', addedConfession);
-        addedConfession.save(function sendJSON(err, amendedAlias) {
-          if(err) {return console.log('ERR4: ', err);}
-          console.log('Successfully added confession and sending amended alias');
-          res.json(amendedAlias);
-        });*/
+      db.Alias.findById(aliasId)
+      .populate('confession')
+      .exec(function (err, foundAlias) {
+        console.log('foundAlias: ', foundAlias);
+        if (err) {return console.log('ERR4: ', err);}
+        else {
+          foundAlias.confessions.push(new db.Confession({submission: req.body.confession }));
+          foundAlias.save();
+          res.json(foundAlias);
+        }
       });
     }
   });
