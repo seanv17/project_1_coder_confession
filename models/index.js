9c5b68dd96fdc7675732d3a614d1c106428225e1
seanvsville <seanvanho@gmail.com>
2016-04-11 10:48:12 -0700

    ready for heroku deploy attempt #1

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/coderconfession");
+mongoose.connect( process.env.MONGOLAB_URI ||
+                  process.env.MONGOHQ_URL ||
+                  "mongodb://localhost/coderconfession" );
 
 module.exports.Alias = require('./alias.js');
 module.exports.Confession = require('./confession.js');
